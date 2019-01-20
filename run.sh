412c4912f8341b2c9ee595c196af54069603b99b
turtlego <turtlego@qq.com>
2019-01-20 15:31:15 +0800

    update

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ echo current dir is $PWD
 export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 
 # 定义各目录
-output_dir=./output  # 训练目录
-dataset_dir=./data # 数据集目录，这里是写死的，记得修改
+output_dir=/output  # 训练目录
+dataset_dir=/data # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train
 checkpoint_dir=$train_dir
@@ -45,4 +45,4 @@ done
 python3 ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
 
 # 在test.jpg上验证导出的模型
-#python3 ./inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
+python3 ./inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
